62464366d6d1f8f3224cccda74b1ee75285426a0
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-04-22 12:55:37 -0400

    Setting up passport auth with google

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express')
-const app = express()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
-app.get('/', (req, res) => {
-    res.send({Name: 'Brian Goldfarb'})
-})
+const app = express();
 
-//Dynamic Port Binding
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback' 
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken)
+        console.log('refresh token', refreshToken)
+        console.log('profile', profile)
+    })
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        //determines what we want from the user, permisions from user
+        //we can also ask for contact list....
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+
+
+//Dynamic Port Binding, Heroku will assign a port 
+//to the environment variable, PORT 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
