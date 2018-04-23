51b996af70bd48654b43333cf1ebfb12fc9472fb
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-04-23 14:13:47 -0400

    serializing user

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,15 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
+//fetch users when one param, this is the model class
+//.save() saves instance of 'User' to database
+const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
 
  passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -10,9 +17,19 @@ const keys = require('../config/keys')
     callbackURL: '/auth/google/callback' 
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken)
-        console.log('refresh token', refreshToken)
-        console.log('profile', profile)
+     User.findOne({ googleId: profile.id})
+        .then((existingUser) => {
+            if(existingUser){
+                //we already have a record with given profile ID
+                done(null, existingUser);
+            } else {
+                //we don't have user record with profile ID
+                //Creating a model instance here
+                new User({googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user))
+            }  
+        })
     })
 );
 
