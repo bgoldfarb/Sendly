cdd8d72bdb6a6874355122782c944a7fa81a68a5
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-05 12:35:28 -0400

    Creating more routes and adding components

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import App from './components/App';
+import reducers from './reducers'
+
+//When this gets some new state, the provider informs all children components
+//that some new state is available and will update new components with new state
+const store = createStore(reducers,{}, applyMiddleware());
+
+//Provider tag reads changes from redux store
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, 
+    document.querySelector('#root')
+);
+
+ 
