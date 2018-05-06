ab216fa6fe31b633c2df36cec0f96c4883ae6f1d
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-06 19:42:52 -0400

    refactoring index

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,7 @@
 //Records whether or not the user is logged in 
 
 export default function(state = {}, action){
+    console.log(action)
     switch(action.type){
         default: 
             return state;
