8b2c9d3fa741a5fd182ed404cda39cc80865a0c0
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-06 18:54:21 -0400

    Setting up redux thruk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 
 import App from './components/App';
 import reducers from './reducers'
+ 
 
 //When this gets some new state, the provider informs all children components
 //that some new state is available and will update new components with new state
-const store = createStore(reducers,{}, applyMiddleware());
+const store = createStore(reducers,{}, applyMiddleware(reduxThunk));
 
 //Provider tag reads changes from redux store
 ReactDOM.render(
