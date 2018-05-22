19623442ce1d2397b70679619542be8a8b1d306c
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-21 22:09:59 -0400

    Adding in body parser middleware

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 const keys = require('./config/keys')
-
+const bodyParser = require('body-parser')
 require('./models/User')
 require('./services/passport')
 
@@ -12,6 +12,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express();
 
+app.use(bodyParser.json()) //any time a request comes into app, this will asign to rect.body.property of incoming request object
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000, //This is in ms
